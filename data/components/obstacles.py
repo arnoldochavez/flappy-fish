141615d8d67fd5fd9f2ecd880b293e7d4d33583a
arnoldochavez <33552997+arnoldochavez@users.noreply.github.com>
2017-11-18 12:06:31 -0700

    Added image

--- data/components/obstacles.py
+++ data/components/obstacles.py
@@ -3,6 +3,7 @@ __author__ = "arnoldochavez"
 import pygame
 from .. import constants as const
 from .. import tools
+from .. import resources as res
 
 class Coral:
 	def __init__( self, x, y ):
@@ -12,12 +13,14 @@ class Coral:
 		self.width = 48
 		self.height = 256
 		self.name = "Coral"
-		self.offsetX = 34
-		self.offsetY = 28
+		self.offsetX = 32
+		self.offsetY = -56
 		self.destroy = False
+		self.angle = 0
 		self.side = const.DIR_UP
 		self.rect = pygame.Rect(self.x - (self.width/2), self.y + self.separation, self.width, self.height)
 		self.control = None
+		self.image = res.IMAGE["CORAL"]
 
 	def update( self ):
 		if self.control.gameState == const.GAMESTATE_RUN:
@@ -27,13 +30,14 @@ class Coral:
 			if self.side == const.DIR_DOWN:
 				self.rect = pygame.Rect(self.x - (self.width/2), self.y + self.separation, self.width, self.height)
 			elif self.side == const.DIR_UP:
+				self.angle = 180
 				self.rect = pygame.Rect(self.x - (self.width/2), self.y - self.height - self.separation, self.width, self.height)
 	
 	def draw( self, surface ):
-		"""tempSurf = pygame.Surface((self.image.get_width() * 2, self.image.get_height() * 2), pygame.SRCALPHA)
+		tempSurf = pygame.Surface((self.image.get_width() * 4, self.image.get_height() * 4), pygame.SRCALPHA)
 		tempSurf.blit(self.image, ((tempSurf.get_width()/2) - self.offsetX, (tempSurf.get_height()/2) - self.offsetY))
 		tempSurf = pygame.transform.rotate(tempSurf, self.angle)
-		surface.blit(tempSurf, (self.x - (tempSurf.get_width()/2), self.y - (tempSurf.get_height()/2)))"""
+		surface.blit(tempSurf, (self.x - (tempSurf.get_width()/2), self.y - (tempSurf.get_height()/2)))
 		pygame.draw.rect(surface, (255,0,0), self.rect, 1)
 		pygame.draw.line(surface, (0,0,255), (self.x-16, self.y), (self.x+16, self.y),1)
 		pygame.draw.line(surface, (0,0,255), (self.x, self.y-16), (self.x, self.y+16),1)
